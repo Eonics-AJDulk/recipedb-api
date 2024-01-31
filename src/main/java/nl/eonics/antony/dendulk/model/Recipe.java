b5622fefc8978447ea86f445ba6b6247efb9b3b5
Antony den Dulk <antony.dendulk@eonics.nl>
2024-01-31 19:33:53 +0100

    Attempt to get Swagger endpoint working

--- src/main/java/nl/eonics/antony/dendulk/model/Recipe.java
+++ src/main/java/nl/eonics/antony/dendulk/model/Recipe.java
@@ -1,8 +1,24 @@
 package nl.eonics.antony.dendulk.model;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
 import java.util.List;
 
-public record Recipe(String name, boolean vegetarian, int servings, List<String> ingredients, String instructions) {
+@ApiModel(description = "A recipe")
+public record Recipe(
+        @ApiModelProperty(value = "The name of the recipe", example = "Pasta") String name,
+        @ApiModelProperty(value = "Whether the recipe is vegetarian", example = "true") Boolean vegetarian,
+        @ApiModelProperty(value = "The number of servings", example = "4") int servings,
+        @ApiModelProperty(value = "The ingredients", example = "[\"pasta\", \"tomato sauce\", \"cheese\"]") List<String> ingredients,
+        @ApiModelProperty(value = "The instructions", example = "1. Cook pasta\n2. Add tomato sauce\n3. Add cheese") String instructions
+) {
+
+    public Recipe {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("Name cannot be null or blank");
+        }
+    }
 
     @Override
     public boolean equals(Object obj) {
@@ -16,4 +32,23 @@ public record Recipe(String name, boolean vegetarian, int servings, List<String>
         return false;
     }
 
+    public Boolean isVegetarian() {
+        return vegetarian;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<String> getIngredients() {
+        return ingredients;
+    }
+
+    public int getServings() {
+        return servings;
+    }
+
+    public String getInstructions() {
+        return instructions;
+    }
 }
